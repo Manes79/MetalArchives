5175f948d7a1e6e674dcd739a11e2b6c3da0a356
Manes79 <mszornel@gmail.com>
2023-01-29 21:49:33 +0100

    first commit

---
+++ src/main/java/pl/manes/metalarchives/MetalArchivesApplication.java
@@ -0,0 +1,13 @@
+package pl.manes.metalarchives;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetalArchivesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetalArchivesApplication.class, args);
+    }
+
+}
